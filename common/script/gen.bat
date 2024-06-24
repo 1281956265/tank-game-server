25e451e72f690d92d130db722e6891c8cac975ae
box <1281956265@qq.com>
2024-06-24 21:45:57 +0800

    给luban生成的配置文件添加ignore 添加luban必备CLASS

--- common/script/gen.bat
+++ common/script/gen.bat
@@ -1,14 +1,14 @@
 set WORKSPACE=.
 set LUBAN_DLL=%WORKSPACE%\Luban\Luban.dll
 set CONF_ROOT=../src/main/resources/DataTables
-set OUT_PUT=../src/main/gen
+set OUT_PUT=../src/main
 
 dotnet %LUBAN_DLL% ^
     -t server ^
     -c java-json ^
     -d json ^
     --conf %CONF_ROOT%\luban.conf ^
-    -x outputDataDir=%OUT_PUT%/json ^
-    -x outputCodeDir=%OUT_PUT%/code
+    -x outputDataDir=%OUT_PUT%/resources/json ^
+    -x outputCodeDir=%OUT_PUT%/gen/cfg
 
 pause
--- common/script/gen.sh
+++ common/script/gen.sh
@@ -3,12 +3,12 @@ WORKSPACE=.
 
 LUBAN_DLL=$WORKSPACE/Luban/Luban.dll
 CONF_ROOT=../src/main/resources/DataTables
-OUT_PUT=../src/main/gen
+OUT_PUT=../src/main
 
 dotnet $LUBAN_DLL \
     -t server \
     -c java-json \
     -d json \
     --conf $CONF_ROOT/luban.conf \
-    -x outputDataDir=$OUT_PUT/json \
-    -x outputCodeDir=$OUT_PUT/code
+    -x outputDataDir=$OUT_PUT/resources/json \
+    -x outputCodeDir=$OUT_PUT/gen/cfg
